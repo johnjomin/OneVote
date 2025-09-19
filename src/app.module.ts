68b0885442cbd10455ebb3154cae6767aa3d2fa2
Jomin John <jominjhn@googlemail.com>
2025-09-20 00:06:20 +0100

    feat: adding filters and interceptors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,35 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_INTERCEPTOR, APP_FILTER } from '@nestjs/core';
+import { PollsModule } from './polls/polls.module';
 import { AppDataSource } from './data-source';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { HttpExceptionFilter, AllExceptionsFilter } from './common/filters/http-exception.filter';
 
 @Module({
   imports: [
-    // Configure TypeORM with data source
+    // Configure TypeORM with our data source
     TypeOrmModule.forRoot(AppDataSource.options),
+
+    // Feature modules
+    PollsModule,
+  ],
+  providers: [
+    // Global logging interceptor for all HTTP requests
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+
+    // Global exception filters for consistent error handling
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
   ],
 })
 export class AppModule {}
