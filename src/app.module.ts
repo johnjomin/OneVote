78d21c083bf83af8ccaf33410736a3fcec5e1e8c
Jomin John <jominjhn@googlemail.com>
2025-09-19 16:36:25 +0100

    chore: update of app module ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from './data-source';
 
-@Module({})
+@Module({
+  imports: [
+    // Configure TypeORM with data source
+    TypeOrmModule.forRoot(AppDataSource.options),
+  ],
+})
 export class AppModule {}
